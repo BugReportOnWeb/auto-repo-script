73dbd4cfd29139236d15e50fe569bf31c4462f19
Dev <flashytesting@gmail.com>
2022-06-21 22:23:58 +0530

    Fix indentation

--- src/main.sh
+++ src/main.sh
@@ -19,13 +19,13 @@ cat <<EOF
 - GitHub URL: https://github.com/$USERNAME
 EOF
 
-    echo; echo -n 'Is this OK [(Y)es/(n)o]? (yes) '
-    read CONFIRM
-    CONFIRM=$(echo $CONFIRM | tr '[:upper:]' '[:lower:]')
+  echo; echo -n 'Is this OK [(Y)es/(n)o]? (yes) '
+  read CONFIRM
+  CONFIRM=$(echo $CONFIRM | tr '[:upper:]' '[:lower:]')
 
-    if [[ $CONFIRM == 'no' || $CONFIRM == 'n' ]]; then
-        user_inputs
-    fi
+  if [[ $CONFIRM == 'no' || $CONFIRM == 'n' ]]; then
+    user_inputs
+  fi
 }
 
 ########################################
@@ -36,39 +36,39 @@ EOF
 #   User input values and run preview()
 ########################################
 user_inputs() {
-    if [[ $AUTO == true ]]; then
-        REPO_NAME=$DIR_NAME
-        DESCRIPTION=''
-        PRIVATE=false
-    else
-        echo -n "Repository Name: ($DIR_NAME) "
-        read REPO_NAME
-        if [[ -z $REPO_NAME ]]; then
-            REPO_NAME=$DIR_NAME
-        fi
-
-        echo -n 'Repository Description: '
-        read DESCRIPTION
-
-        # o == Open/Public && c == Close/Private
-        echo -n "Repository Visibility [(O)pen/(c)lose]: (Public) "
-        read VISIBILITY
-        VISIBILITY=$(echo $VISIBILITY | tr '[:upper:]' '[:lower:]')
-        if [[ -z $VISIBILITY || $VISIBILITY == 'o' ]]; then
-            PRIVATE=false
-            STATUS='Open/Public'
-        else
-            PRIVATE=true
-            STATUS='Close/Private'
-        fi
+  if [[ $AUTO == true ]]; then
+    REPO_NAME=$DIR_NAME
+    DESCRIPTION=''
+    PRIVATE=false
+  else
+    echo -n "Repository Name: ($DIR_NAME) "
+    read REPO_NAME
+    if [[ -z $REPO_NAME ]]; then
+      REPO_NAME=$DIR_NAME
     fi
 
-    echo -n 'GitHub Username: '
-    read USERNAME
+    echo -n 'Repository Description: '
+    read DESCRIPTION
 
-    echo -n "Host password for '$USERNAME': "
-    read -s PASSWORD; echo -e '\n'
-    preview
+    # o == Open/Public && c == Close/Private
+    echo -n "Repository Visibility [(O)pen/(c)lose]: (Public) "
+    read VISIBILITY
+    VISIBILITY=$(echo $VISIBILITY | tr '[:upper:]' '[:lower:]')
+    if [[ -z $VISIBILITY || $VISIBILITY == 'o' ]]; then
+      PRIVATE=false
+      STATUS='Open/Public'
+    else
+      PRIVATE=true
+      STATUS='Close/Private'
+    fi
+  fi
+
+  echo -n 'GitHub Username: '
+  read USERNAME
+
+  echo -n "Host password for '$USERNAME': "
+  read -s PASSWORD; echo -e '\n'
+  preview
 }
 
 ########################################
@@ -81,23 +81,23 @@ user_inputs() {
 #   --no-push to not push files after setup()
 ########################################
 flags() {
-    case $1 in
-        -h|--help)
-            echo 'Help page' 
-            ;;
-        -y|--yes)
-            AUTO=true
-            ;;
-        --no-push)
-            PUSH=false
-            ;;
-        *)
-            if [[ -n $1 ]]; then
-                echo "$0: cannot use $1: Invalid option" >&2
-                exit 1
-            fi
-            ;;
-    esac
+  case $1 in
+    -h|--help)
+      echo 'Help page' 
+      ;;
+    -y|--yes)
+      AUTO=true
+      ;;
+    --no-push)
+      PUSH=false
+      ;;
+    *)
+      if [[ -n $1 ]]; then
+        echo "$0: cannot use $1: Invalid option" >&2
+        exit 1
+      fi
+      ;;
+  esac
 }
 
 ########################################
@@ -109,17 +109,16 @@ flags() {
 #   title and an empty .gitignore
 ########################################
 setup() {
-    # Quick repository setup
-    git init
-    echo "# $REPO_NAME" >> README.md
-    touch .gitignore
-    git add .
-    git commit -m "Initial commit"
-    git remote add origin https://github.com/$USERNAME/$REPO_NAME.git
+  git init
+  echo "# $REPO_NAME" >> README.md
+  touch .gitignore
+  git add .
+  git commit -m "Initial commit"
+  git remote add origin https://github.com/$USERNAME/$REPO_NAME.git
 
-    if [[ $PUSH == true ]]; then
-        git push -u origin master
-    fi
+  if [[ $PUSH == true ]]; then
+    git push -u origin master
+  fi
 }
 
 ########################################
@@ -131,27 +130,27 @@ setup() {
 #   stderr fro cURL && git initilization
 ########################################
 main() {
-    DIR_NAME=$(basename $(pwd))
-    AUTO=false
-    PUSH=true
-    EXIT=false
+  DIR_NAME=$(basename $(pwd))
+  AUTO=false
+  PUSH=true
+  EXIT=false
 
-    flags $1
-    user_inputs; echo
+  flags $1
+  user_inputs; echo
 
-    # GitHub API call using cURL
-    curl \
-        -X POST \
-        -H "Accept: application/vnd.github.v3+json" \
-        -H "Authorization: token $PASSWORD" \
-        -d "{ \
-            \"name\": \"$REPO_NAME\", \
-            \"description\": \"$DESCRIPTION\", \
-            \"private\": $PRIVATE \
-        }" \
-        https://api.github.com/user/repos >> /dev/null; echo
+  # GitHub API call using cURL
+  curl \
+    -X POST \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token $PASSWORD" \
+    -d "{ \
+      \"name\": \"$REPO_NAME\", \
+      \"description\": \"$DESCRIPTION\", \
+      \"private\": $PRIVATE \
+    }" \
+    https://api.github.com/user/repos >> /dev/null; echo
 
-    setup
+  setup
 }
 main $1
 
