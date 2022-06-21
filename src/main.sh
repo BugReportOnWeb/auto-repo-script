d802ab3e7a822f4c5a61c3502ee77bab67f7f24a
Dev <flashytesting@gmail.com>
2022-06-22 00:52:51 +0530

    Separate constants

--- src/main.sh
+++ src/main.sh
@@ -4,12 +4,15 @@
 # Date: 20/06/2022
 
 # Global Constants
-DIR_NAME="$(basename $(pwd))"
+REPO_NAME="$(basename $(pwd))"
 CMD_NAME="$(basename $0)"
+STATUS='Open/Public'
+DESCRIPTION=''
 
-AUTO=false
-PUSH=true
-EXIT=false
+PUSH='true'
+AUTO='false'
+EXIT='false'
+PRIVATE='false'
 
 RED='\033[0;31m'
 NC='\033[0m'
@@ -19,24 +22,26 @@ NORM="$(tput sgr0)"
 ########################################
 # Preview the options from user inputs.
 # Globals:
-#   REPONAME, DESCRIPTION, STATUS, USERNAME
+#   REPONAME, DESCRIPTION, STATUS, username
 # Outputs:
 #   Details preview and step over user_input()
 #   if affirmative CONFIRM, else recurse preview()
 ########################################
 preview() {
+  local confirm
+
 cat <<EOF
 - Repository Name: ${REPO_NAME}
 - Repository Description: ${DESCRIPTION}
 - Repository Visibility: ${STATUS}
-- GitHub URL: https://github.com/${USERNAME}
+- GitHub URL: https://github.com/${username}
 EOF
 
   echo; echo -n 'Is this OK [(Y)es/(n)o]? (yes) '
-  read CONFIRM
-  CONFIRM=$(echo "${CONFIRM}" | tr '[:upper:]' '[:lower:]')
+  read confirm
+  confirm=$(echo "${confirm}" | tr '[:upper:]' '[:lower:]')
 
-  if [[ "${CONFIRM}" == 'no' || "${CONFIRM}" == 'n' ]]; then
+  if [[ "${confirm}" == 'no' || "${confirm}" == 'n' ]]; then
     user_inputs
   fi
 }
@@ -49,38 +54,32 @@ EOF
 #   User input values and run preview()
 ########################################
 user_inputs() {
-  if [[ "${AUTO}" == true ]]; then
-    REPO_NAME="${DIR_NAME}"
-    DESCRIPTION=''
-    PRIVATE=false
-  else
-    echo -n "Repository Name: (${DIR_NAME}) "
+  if [[ "${AUTO}" == 'false' ]]; then
+    local visibility
+
+    echo -n "Repository Name: (${REPO_NAME}) "
     read REPO_NAME
     if [[ -z "${REPO_NAME}" ]]; then
-      REPO_NAME="${DIR_NAME}"
+      REPO_NAME="$(basename $(pwd))"
     fi
 
     echo -n 'Repository Description: '
     read DESCRIPTION
 
-    # o == Open/Public && c == Close/Private
     echo -n "Repository Visibility [(O)pen/(c)lose]: (Public) "
-    read VISIBILITY
-    VISIBILITY=$(echo "${VISIBILITY}" | tr '[:upper:]' '[:lower:]')
-    if [[ -z "${VISIBILITY}" || "${VISIBILITY}" == 'o' ]]; then
-      PRIVATE=false
-      STATUS='Open/Public'
-    else
-      PRIVATE=true
+    read visibility
+    visibility=$(echo "${visibility}" | tr '[:upper:]' '[:lower:]')
+    if [[ "${visibility}" == 'c' ]]; then
+      PRIVATE='true'
       STATUS='Close/Private'
     fi
   fi
 
   echo -n 'GitHub Username: '
-  read USERNAME
+  read username
 
-  echo -n "Host password for '${USERNAME}': "
-  read -s PASSWORD; echo -e '\n'
+  echo -n "Host password for '${username}': "
+  read -s password; echo -e '\n'
   preview
 }
 
@@ -100,8 +99,8 @@ flags() {
     -h|--help)
       echo 'Help page' 
       ;;
-    -y|--yes) AUTO=true ;;
-    --no-push) PUSH=false ;;
+    -y|--yes) AUTO='true' ;;
+    --no-push) PUSH='false' ;;
     *)
       if [[ -n "$1" ]]; then
         echo -e "${BOLD}${CMD_NAME}${NORM} \
@@ -117,7 +116,7 @@ Try '${CMD_NAME} --help' for more information." >&2
 ########################################
 # A quick remote repository setup
 # Globals:
-#   REPO_NAME, USERNAME, PUSH
+#   REPO_NAME, username, PUSH
 # Returns:
 #   README.md file with repo name as the
 #   title and an empty .gitignore
@@ -128,9 +127,9 @@ setup() {
   touch .gitignore
   git add .
   git commit -m "Initial commit"
-  git remote add origin https://github.com/${USERNAME}/${REPO_NAME}.git
+  git remote add origin https://github.com/${username}/${REPO_NAME}.git
 
-  if [[ "${PUSH}" == true ]]; then
+  if [[ "${PUSH}" == 'true' ]]; then
     git push -u origin master
   fi
 }
@@ -139,7 +138,7 @@ setup() {
 # The main function that goes through
 # flags > user_inputs > cURL > setup
 # Globals:
-#   PASSWORD, REPO_NAME, DESCIPTION, PRIVATE
+#   password, REPO_NAME, DESCIPTION, PRIVATE
 # Arguments:
 #   Available flags that goes to flags()
 # Returns:
@@ -153,7 +152,7 @@ main() {
   curl \
     -X POST \
     -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token ${PASSWORD}" \
+    -H "Authorization: token ${password}" \
     -d "{ \
       \"name\": \"${REPO_NAME}\", \
       \"description\": \"${DESCRIPTION}\", \
